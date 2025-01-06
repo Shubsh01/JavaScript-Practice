54a3db677fcf8716ec58967ce7c587be01f41706
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-01-06 09:17:19 +0000

    Concersion of Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+// Number
+let score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// 33 => Number
+// "33" => String
+// "33abc" => NaN (not a number)
+// true/false => boolean
+
+// Boolean
+let isBoolean = "Shubhang"
+let isLoggedIn = Boolean(isBoolean)
+console.log(typeof isLoggedIn);
+console.log(isLoggedIn)
+
+// "" => false
+// "Shubhang" => true
+
+let someNumber = 21
+let stringToNumber = String(someNumber)
+console.log(typeof stringToNumber);
+console.log(stringToNumber);
+
