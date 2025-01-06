8d9552bfa71988158422a868342933f181ae7a5a
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-01-06 08:47:52 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const name = "Shubhang Sharma"
+const accountNumber = "12345"
+let accountMail = "khuchbhi@gmail.com"
+var accountPassword = "12321"
+accountPassword = 55555
+
+console.table([name, accountMail,accountNumber,accountPassword])
+
+
+
+console.log(accountPassword);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// Number
+// BigInt
+// String
+// Boolean
+// null => Stand alone value, not equal to zero
+// undefined
+// symbol => unique
+
+// Object
+
+console.log(typeof "shubhang")
+console.log(typeof null);
+console.log(typeof undefined);
