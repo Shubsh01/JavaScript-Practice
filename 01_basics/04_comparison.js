4d19b23d91721cee72048c57fc4cfb5b199a4786
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-01-08 02:57:24 +0000

    comparison basics

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log("2">1);
+console.log(null>0);
+console.log(0>null);
+console.log(undefined>0);
+console.log(0>undefined);
+console.log(null>=0);
+console.log("2"==2);
+console.log("2"===2);
+console.log(null==0);
+
+
+
+
