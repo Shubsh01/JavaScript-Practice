896529cb08960560d622c642cc5e51580a41b261
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-01-12 05:58:02 +0000

    Arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel = ["spiderman","Ironman","DrStrange"]
+const dc = ["flash","superman","batman"]
+
+// marvel.push(dc);
+// console.log(marvel);
+
+// console.log(marvel[3][1]);
+
+const indian = ["Shaktiman"]
+const all_heros = [...dc,...marvel,...indian]
+console.log(all_heros);
+
+const random_array = [1,2,3,[4,5],6,7,[8,9,[10,11,12]]]
+const useable_array = random_array.flat(1)
+console.log(useable_array);
+
+console.log(Array.isArray("Shubhang"))
+console.log(Array.from("Shubhang"));
+console.log(Array.from({name:"Shubhang"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
