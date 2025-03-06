990b1edf6ffae840ebd66b5329bd260d7d7716e2
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-03-06 10:50:30 +0000

    ASYNC AWAIT

--- 12_advanceOne/02_promises.js
+++ 12_advanceOne/02_promises.js
@@ -61,3 +61,77 @@ promiseFour.then(function(user){
 }).catch((error)=>{
     console.log(error);
 }).finally(()=> console.log("The promise is either resolved or rejected"))
+
+
+// Async Await 
+// it is used by calling async function that we create 
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        let error = false
+        if(!error){
+            resolve({username:"js", password:"123"})
+        }else{
+            reject('Error occured in JS')
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+    finally{
+        console.log('khuch toh hua h');
+    }
+}
+
+consumePromiseFive()
+
+
+// FETCH 
+// Async function gets a response 
+// fetch gets response from the url and it takes time for it to get the response so {{we have to add await}} so that it gets the response 
+
+async function getAllUsers() {
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        // here the conversion of response to json also takes time, so we have to add await here also 
+        const data = await response.json()
+        console.log(data);
+    }
+    catch(error){
+        console.log("E :", error);
+    }
+}
+
+// getAllUsers()
+
+
+// using .then() and catch()
+const response = fetch("https://jsonplaceholder.typicode.com/users")
+response
+.then((getAllUsers)=>{
+        // here return is imp for it to transfer values to second.then()
+    return getAllUsers.json()
+})
+.then((data)=>console.log(data))
+.catch((error)=>console.log(error))
+
+
+// IT CAN ALSO BE WRITTEN AS 
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    // here return is imp for it to transfer values to second.then()
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
