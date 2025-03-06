51bbf23c953ca029a4b7db7af17be9425012076c
Shubsh01 <91937074+Shubsh01@users.noreply.github.com>
2025-03-06 16:10:39 +0000

    Promises and fetch

--- 12_advanceOne/02_promises.js
+++ 12_advanceOne/02_promises.js
@@ -135,3 +135,10 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .catch((error)=>{
     console.log(error);
 })
+
+// FETCH IMP POINT 
+
+// A fetch() promise only rejects when a network error is encountered
+// (which is usually when there's a permissions issue or similar). A fetch()
+// promise does not reject on HTTP errors (404, etc.). Instead, a then ()
+// handler must check the Response.ok and/or Response.status properties.
